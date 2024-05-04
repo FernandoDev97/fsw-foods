ca521d2556038e688f130f64d57134552cb223e9
FernandoDev97 <l.fernando.dev@gmail.com>
2024-05-03 22:05:25 -0300

    add promo banner home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { CategoryList } from './_components/category-list'
 import { Header } from './_components/header'
 import { Search } from './_components/search'
@@ -11,6 +13,17 @@ export default function Home() {
       </div>
       <div className="px-5 mt-6">
         <CategoryList />
+      </div>
+
+      <div className="mt-6 px-5">
+        <Image
+          src="/bannerPizza.png"
+          alt="Banner que idica ate 30% de desconto com um fundo vermelho de uma pizza"
+          width={0}
+          height={0}
+          sizes="100vh"
+          className="w-full h-auto object-contain"
+        />
       </div>
     </div>
   )
