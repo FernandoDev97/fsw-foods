ab2e9d388f5008ff772d82387dc2e10107af9e46
FernandoDev97 <l.fernando.dev@gmail.com>
2024-05-18 21:31:41 -0300

    fix button back

--- app/_components/button-back.tsx
+++ app/_components/button-back.tsx
@@ -9,7 +9,7 @@ export const ButtonBack = () => {
   const router = useRouter()
   return (
     <Button
-      onClick={() => router.replace('/')}
+      onClick={() => router.back()}
       className="absolute bg-gray-100 top-4 left-4 h-auto p-1 rounded-full text-center hover:bg-white"
     >
       <ChevronLeft className="text-black" />
