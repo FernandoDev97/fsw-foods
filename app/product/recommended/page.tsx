1812243df835302b5d4993b0f0aa3aabdfb71759
FernandoDev97 <l.fernando.dev@gmail.com>
2024-05-20 22:46:52 -0300

    add page to product recommended

---
+++ app/product/recommended/page.tsx
@@ -0,0 +1,35 @@
+import { Header } from '@/app/_components/header'
+import { ProductItem } from '@/app/_components/product-item'
+import { prismaClient } from '@/app/_lib/prisma'
+
+const RestauratsRecommendeds = async () => {
+  const products = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 30,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  })
+  return (
+    <>
+      <Header />
+      <h1 className="px-5 mt-8 font-semibold text-xl">Pedidos recomendados.</h1>
+
+      <div className="grid grid-cols-2 mt-4 sm:grid-cols-3 md:grid-cols-4 gap-4 px-5">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default RestauratsRecommendeds
